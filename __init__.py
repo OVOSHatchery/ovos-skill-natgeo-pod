5480123d669227593717fc83ff36d994a6b9fa35
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2024-03-06 19:17:32 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,19 @@
-from mycroft import MycroftSkill, intent_file_handler, intent_handler
-import requests
-from mycroft.skills.core import resting_screen_handler
-from adapt.intent import IntentBuilder
-from mtranslate import translate
 from datetime import datetime, timedelta
 
+import requests
+from ovos_workshop.decorators import intent_handler
+from ovos_workshop.decorators import resting_screen_handler
+from ovos_workshop.intents import IntentBuilder
+from ovos_workshop.skills import OVOSSkill
 
-class NatGeoPictureOfThedaySkill(MycroftSkill):
+
+class NatGeoPictureOfThedaySkill(OVOSSkill):
 
     def update_picture(self):
         try:
-            today = datetime.now().replace(hour=12, second=0, minute=0,
+            today = datetime.now().replace(hour=12,
+                                           second=0,
+                                           minute=0,
                                            microsecond=0)
             if not self.settings.get("ts"):
                 self.settings["ts"] = (today - timedelta(days=1)).timestamp()
@@ -25,8 +28,8 @@ class NatGeoPictureOfThedaySkill(MycroftSkill):
                 title = data['items'][0]['image']['title']
                 summary = data['items'][0]['image']['caption']
                 if not self.lang.lower().startswith("en"):
-                    summary = translate(summary, self.lang)
-                    title = translate(title, self.lang)
+                    summary = self.translator.translate(summary, self.lang)
+                    title = self.translator.translate(title, self.lang)
 
                 self.settings['imgLink'] = data['items'][0]['image']['uri']
                 self.settings['title'] = title
@@ -45,7 +48,7 @@ class NatGeoPictureOfThedaySkill(MycroftSkill):
         self.gui.clear()
         self.gui.show_page('idle.qml')
 
-    @intent_file_handler('natgeo.intent')
+    @intent_handler('natgeo.intent')
     def handle_pod(self, message):
         self.update_picture()
         self.gui.clear()
@@ -55,11 +58,8 @@ class NatGeoPictureOfThedaySkill(MycroftSkill):
 
         self.speak(self.settings['title'])
 
-    @intent_handler(IntentBuilder("ExplainIntent")
-                    .require("ExplainKeyword").require("NatGeo"))
+    @intent_handler(
+        IntentBuilder("ExplainIntent").require("ExplainKeyword").require(
+            "NatGeo"))
     def handle_explain(self, message):
         self.speak(self.settings['summary'])
-
-
-def create_skill():
-    return NatGeoPictureOfThedaySkill()
