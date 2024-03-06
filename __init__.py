66b0a84541b59a6819423ce02d594342637ed2bd
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2024-03-06 19:29:11 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,3 @@
-from datetime import datetime, timedelta
-
 import requests
 from ovos_workshop.decorators import intent_handler
 from ovos_workshop.decorators import resting_screen_handler
@@ -11,30 +9,18 @@ class NatGeoPictureOfThedaySkill(OVOSSkill):
 
     def update_picture(self):
         try:
-            today = datetime.now().replace(hour=12,
-                                           second=0,
-                                           minute=0,
-                                           microsecond=0)
-            if not self.settings.get("ts"):
-                self.settings["ts"] = (today - timedelta(days=1)).timestamp()
-            if today.timestamp() != self.settings["ts"] or \
-                    not self.settings.get('imgLink'):
-                baseurl = "https://www.nationalgeographic.com/photography/photo-of-the-day" \
-                          "/_jcr_content/.gallery."
-                url = baseurl + str(today.year) + "-" + str(today.month) + ".json"
-
-                data = requests.get(url).json()
-
-                title = data['items'][0]['image']['title']
-                summary = data['items'][0]['image']['caption']
+            url = "https://www.natgeotv.com/ca/photo-of-the-day"
+            data = requests.get(url).text
+            for c in data.split('class="PODItem"')[1:]:
+                title = "National Geographic Photo of the day"
+                url = c.split('src="')[1].split('"')[0]
+                summary = c.split('class="ItemDescription">')[1].split('</div>')[0]
                 if not self.lang.lower().startswith("en"):
                     summary = self.translator.translate(summary, self.lang)
                     title = self.translator.translate(title, self.lang)
-
-                self.settings['imgLink'] = data['items'][0]['image']['uri']
+                self.settings['imgLink'] = url
                 self.settings['title'] = title
                 self.settings['summary'] = summary
-                self.settings["ts"] = today.timestamp()
         except Exception as e:
             self.log.exception(e)
         self.gui['imgLink'] = self.settings['imgLink']
@@ -45,18 +31,15 @@ class NatGeoPictureOfThedaySkill(OVOSSkill):
     @resting_screen_handler("NatGeo")
     def idle(self, message):
         self.update_picture()
-        self.gui.clear()
         self.gui.show_page('idle.qml')
 
     @intent_handler('natgeo.intent')
     def handle_pod(self, message):
         self.update_picture()
-        self.gui.clear()
         self.gui.show_image(self.settings['imgLink'],
                             caption=self.settings['title'],
                             fill='PreserveAspectFit')
-
-        self.speak(self.settings['title'])
+        self.speak("National Geographic Photo of the day")
 
     @intent_handler(
         IntentBuilder("ExplainIntent").require("ExplainKeyword").require(
